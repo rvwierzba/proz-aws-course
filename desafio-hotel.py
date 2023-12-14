70c09f66b9c2efa290b8ee475b01fc126119dcde
Rafa Wierzba <rafaelwierzba@gmail.com>
2023-12-14 12:20:05 -0300

    Update desafio-hotel.py

--- desafio-hotel.py
+++ desafio-hotel.py
@@ -1,6 +1,18 @@
+/*
+Instruções do projeto
+Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel é supersticioso e optou por não ter um 13ro andar.
+
+Escreva um código que imprima todos os números exceto o número 13.
+Escreva mais um código que resolva o mesmo problema, mas dessa vez usando o laço de repetição 'while'.
+
+Como desafio, imprima eles em ordem decrescente (20, 19, 18...)
+
+Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e compartilhe o link desse projeto no campo ao lado para que outros desenvolvedores possam analisá-lo.
+*/
+
 i = 20
 while i != 0:
    if i != 13:
-    print(i) 
+    print(i, "º ANDAR") 
 
    i = i - 1 
