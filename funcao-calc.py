ca7cd030c4238d40c0dd4fe1caae5bac65b182a6
Rafa Wierzba <rafaelwierzba@gmail.com>
2023-12-14 12:56:23 -0300

    Create funcao-calc.py

---
+++ funcao-calc.py
@@ -0,0 +1,22 @@
+# op=1 => ADIÇÂO | op=2 => SUBTRAÇÂO | op=3 => MULTIPLICAÇÂO | op=4 => DIVISÂO
+
+def calc(n1, n2 ,op):
+  if op == 1:
+    return n1 + n2
+  elif op == 0:
+    return n1 - n2
+  elif op == 3:
+    return n1 * n2
+  elif op == 4:
+    return n1 / n2    
+     
+
+    
+
+
+
+
+
+
+
+
