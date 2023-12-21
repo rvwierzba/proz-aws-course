ce5ac888d54a7840c0dec3e047dddc387d887780
Rafa Wierzba <rafaelwierzba@gmail.com>
2023-12-21 09:45:53 -0300

    Create print-arrays.py

---
+++ print-arrays.py
@@ -0,0 +1,10 @@
+def printArrays():
+  arrProdutosInformatica = ["Mouse", "Teclado", "Mousepad", "Monitor", "SSD"]
+  arrAnosNascimento = [1999, 1995, 1977, 1975, 2002]
+  for i in arrProdutosInformatica:
+    print("PRODUTOS: " + i)
+  print("\n")
+  for j in arrAnosNascimento:
+   print("ANOS DE NASCIMENTO: " + str(j))
+
+printArrays()    
