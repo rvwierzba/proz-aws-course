44c4383d98c8d6dac9d522d9ae0ed6a9628d3738
Rafa Wierzba <rafaelwierzba@gmail.com>
2024-01-08 13:35:27 -0300

    Create products-for-sale-toList.py

---
+++ products-for-sale-toList.py
@@ -0,0 +1,4 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+for product in lista_produtos:
+  print("Temos " + product + " à venda!")
